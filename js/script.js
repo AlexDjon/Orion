36c78396893441c7a0f1d2bade29bb7ca902537c
AlexDjon <alexdjonata30@gmail.com>
2020-01-29 18:43:00 -0300

    Max. Resolutions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
    $('.historias .grid b').on('click', function() {
-      const status = $('#p2').css('display') == 'none' ? 'flex' : 'none'
-      $('#p2').css('display', status)
+      const status = $('#p2').css('display')
+      if(status == 'none') {
+         $('#p2').css('display', 'flex')
+         $('.historias .grid b').html('..menos')
+      } else {
+         $('#p2').css('display', 'none')         
+         $('.historias .grid b').html('..mais')
+      }
    })
 })
