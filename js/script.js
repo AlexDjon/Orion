809fd2e87f96b2649b1e9f5f270694a0edc98c3d
AlexDjon <alexdjonata30@gmail.com>
2020-01-30 03:03:21 -0300

    Modificações Finais

--- js/script.js
+++ js/script.js
@@ -9,4 +9,16 @@ $(document).ready(function() {
          $('.historias .grid b').html('..mais')
       }
    })
+   $(window).scroll(function(e) {
+      const windowScroll = $(window).scrollTop()
+      if(windowScroll >= 580) {
+         $('.gotop').fadeIn(600)
+      }
+      else {
+         $('.gotop').fadeOut(600)
+      }
+   })
+   $('.gotop').on('click', function() {
+      $(window).scrollTop(0)
+   })
 })
