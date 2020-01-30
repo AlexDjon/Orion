f481ab2bf625009c232ff5f9b9af0fceb5146f4f
AlexDjon <alexdjonata30@gmail.com>
2020-01-30 00:16:05 -0300

    Perfil

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
    $('.historias .grid b').on('click', function() {
       const status = $('#p2').css('display')
       if(status == 'none') {
-         $('#p2').css('display', 'flex')
+         $('#p2').slideToggle(400)
          $('.historias .grid b').html('..menos')
       } else {
-         $('#p2').css('display', 'none')         
+         $('#p2').slideToggle(400)    
          $('.historias .grid b').html('..mais')
       }
    })
